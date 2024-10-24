57fd421a9772ea6baaa2b478df7b3c5e0cf08e6f
Rahul Kumar <rahulkumar8864084963@gmail.com>
2024-10-24 22:29:04 +0530

    scroll down hide

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Dock, DockIcon } from "~/components/ui/dock";
 import { ModeToggle } from "~/components/mode-toggle";
 import { buttonVariants } from "~/components/ui/button";
@@ -12,10 +14,23 @@ import Link from "next/link";
 import { DATA } from "~/constants/profile-data";
 import { BorderBeam } from "./ui/border-beam";
 
+import { motion, useScroll, useMotionValueEvent } from "framer-motion"
+import { useState } from "react";
+
 export default function Navbar() {
+  const { scrollY } = useScroll();
+  const [hide, setHide] = useState(true)
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const next = scrollY.getPrevious();
+    setHide(next! > latest)
+  })
   return (
-    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
-      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div>
+    <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14"  >
+      <div className="fixed bottom-0 inset-x-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background" />
+       <motion.div className="mx-auto"
+        initial={{ y: 100 }}
+        animate={{ y: hide ? 0 : 100 }}
+        transition={{ type: 'spring', stiffness: 300 }}>
         <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
           {DATA.navbar.map(item => (
             <DockIcon key={item.href}>
@@ -74,6 +89,7 @@ export default function Navbar() {
         </DockIcon>
         <BorderBeam size={50} duration={10} delay={9} />
       </Dock>
+       </motion.div>
     </div>
   );
 }
