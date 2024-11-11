11a44e55e6acd30b824babd732327806dc24c1bd
Rahul Kumar <rahulkumar8864084963@gmail.com>
2024-11-11 11:05:37 +0530

    build error fix

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import { ThemeProvider as NextThemeProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemeProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemeProvider {...props}>{children}</NextThemeProvider>
